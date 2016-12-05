c0cfd710f71810e43fbcbe1b37e989e92543dc5a
Han Lazarus <han.lazarus@gmail.com>
2016-12-04 20:24:39 -0800

    Trying to use a generator in a not-working way

--- flyaway.js
+++ flyaway.js
@@ -5,9 +5,9 @@ let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
 
 canvas.width = 0.9*document.documentElement.clientWidth;
-canvas.height = 0.7*document.documentElement.clientHeight;
+canvas.height = 0.8*document.documentElement.clientHeight;
 let canvasPlacement = canvas.getBoundingClientRect();
-let padding = 5;
+let padding = 60;
 let w = canvas.width - padding;
 let h = canvas.height - padding;
 
@@ -19,25 +19,42 @@ context.fillStyle = 'gray';
 let xStart = 250;
 let yStart = 250;
 let angle = Math.random() * 2 * Math.PI;
-let speed = 5; 
-let size = 6;
+let speed = 10; 
+let size = 100;
+
+//Fly image drawing
+let flyImage = new Image();
+flyImage.src = 'fly-4.png'//'drosophilia6sprites-nobg.png';
+let spriteWidth = 100;//200; //amount to increment through sprite sheet
+let flyImageWidth = 400; //1200;
+//Source image info
+let sx = 0; // position in sprite sheet
+let sy = 0;
+let sWidth = 100;//200;
+let sHeight = 100;//260;
+
+//Destination
+let dx = xStart;
+let dy = yStart;
+let dWidth = size;
+let dHeight = size * dy / dx;
 
 //Add buzzy noise to flight path
-let buzz = speed * 0.2;
+let buzz = speed * 0.3;
 let buzzAngle = 0;
 let buzzStep = Math.PI / 5;
 let buzzCounter = 100;
 let directionCounter = 300;
 
-let fly = { x: xStart, y: yStart, angle: angle, speed: speed, size: size, buzzAngle: buzzAngle };
+let fly = { x: xStart, y: yStart, angle: angle, speed: speed, size: size, buzzAngle: buzzAngle, turning: turnFly() };
 
 //Animation speed
 let drawTime = Date.now();
 let elapsed;
-let interval = 5;
+let interval = 30;
 
 //Interactivity
-let swattingDist = speed * 30;
+let swattingDist = 150;
 let chaseAngle;
 let x_diff;
 let y_diff;
@@ -45,18 +62,30 @@ let mouseX, mouseY;
 let escapeRotation = 1;
 
 function drawFly(){
+    console.log(fly.angle);
     context.strokeStyle = 'gray';
     context.lineWidth = 3;
     context.fillStyle = 'white';
-    context.fillRect(padding, padding, w, h);
-    context.strokeRect(padding, padding, w, h);
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.strokeRect(0, 0, canvas.width, canvas.height);
 
-    context.beginPath();    
-    context.fillStyle = 'gray';
-    context.arc(fly.x, fly.y, fly.size, 0, 2*Math.PI);
-    context.closePath();
-    context.stroke();
+    //Draw fly sprite
+    sx = sx % flyImageWidth;
+    sx += spriteWidth;
+    context.save();
+    context.translate(fly.x + sWidth/2, fly.y + sHeight/2);
+    context.rotate(fly.angle + Math.PI);
+    context.drawImage(flyImage, sx, sy, sWidth, sHeight, 0, 0, dWidth, dHeight);
+    context.restore();
 
+    // //Fake fly
+    // context.beginPath();    
+    // context.fillStyle = 'gray';
+    // context.arc(fly.x, fly.y, fly.size, 0, 2*Math.PI);
+    // context.closePath();
+    // context.stroke();
+
+    // Draw the geometry
     // context.beginPath();
     // context.moveTo(fly.x, fly.y);
     // context.lineTo(fly.x + x_diff, fly.y);
@@ -84,7 +113,6 @@ function drawFly(){
     fly.buzzAngle += buzzStep;
     buzzCounter--;
     directionCounter--;
-    console.log(directionCounter);
 
     //Randomize the buzz noise
     if(buzzCounter < 0){
@@ -95,13 +123,14 @@ function drawFly(){
 
     //Sometimes the fly changes direction without hitting a wall or mouse interaction
     if(directionCounter < 0){
-        fly.angle = Math.random() * Math.PI * 2;
+        fly.turning = turnFly();
         directionCounter = Math.floor(Math.random() * canvas.height / 2);
     }
+    fly.angle = fly.turning.next().value || fly.angle;
 
     //If the mouse is near, fly speeds up and changes direction to evade
-    let dist = ((fly.x - mouseX)**2 + (fly.y - mouseY)**2)**(1/2);
-    if(dist < swattingDist){ 
+    let mouseDist = ((fly.x - mouseX)**2 + (fly.y - mouseY)**2)**(1/2);
+    if(mouseDist < swattingDist){ 
         flyFromMouse();
     }else{ 
         fly.speed = speed; 
@@ -111,13 +140,22 @@ function drawFly(){
 
     //Fly changes direction randomly if it hits a wall in range of pi away from wall
     if(fly.x < padding && Math.cos(fly.angle) < 0){
-        newAngle(-Math.PI/2);
+        fly.x = padding;
+        fly.angle = turnFly();
+        //newAngle(-Math.PI/2);
+
     }else if(fly.x > w && Math.cos(fly.angle) > 0){
-        newAngle(Math.PI/2);
+        fly.x = w;
+        fly.angle = turnFly();
+        //newAngle(Math.PI/2);
     }else if(fly.y < padding && Math.sin(fly.angle) < 0){
-        newAngle(0);
+        fly.y = padding;
+        fly.angle = turnFly();
+        //newAngle(0);
     }else if(fly.y > h && Math.sin(fly.angle) > 0){
-        newAngle(Math.PI);
+        fly.x = h;
+        fly.angle = turnFly();
+        //newAngle(Math.PI);
     } 
 }
 
@@ -132,23 +170,31 @@ function flyFromMouse(){
     x_diff = mouseX - fly.x;
     y_diff = mouseY - fly.y;
     chaseAngle = Math.atan2((y_diff),(x_diff));
-    //Flies tend to fly at a pi/2 angle from attack
-    //
+    //Flies tend to fly at a pi/2 angle from attack, 
+    //escapeRotation is +/-1 randomly reset after out of range
     fly.angle = chaseAngle + Math.PI/2 * escapeRotation;
     //Too slow and fly goes in loops around mouse.
     //Too fast and fly goes off canvas.
-    fly.speed = 10*speed + 100/(x_diff**2 + y_diff**2);
-    buzz = 0;
-    buzzAngle = 0;
-    //have reset to keep these from winding up?
-    //have a pause on the counters?
-    buzzCounter++;
-    directionCounter++;
+    fly.speed = 10*speed + 50/(x_diff**2 + y_diff**2);
 }
 
-//New angle in a range of pi radians, with lower limit of angleOffset argument 
-function newAngle(angleOffset){
-    fly.angle = Math.random() * Math.PI + angleOffset;
+// //New angle in a range of pi radians, with lower limit of angleOffset argument 
+// function newAngle(angleOffset){
+//     fly.angle = Math.random() * Math.PI + angleOffset;
+// }
+
+//Turn fly in a series of steps
+function* turnFly(){
+    //amount to turn
+    let steps = 6;
+    let turnAmount = Math.random() * Math.PI;
+    let increment = Math.PI / steps;
+    //rotation of turn
+    let rotation = Math.round(Math.random()) * 2 - 1;
+    for(let i = 0; i * increment < turnAmount; i++){
+        fly.angle += rotation * increment;
+        yield fly.angle;
+    }
 }
 
 function animateCallback(){
